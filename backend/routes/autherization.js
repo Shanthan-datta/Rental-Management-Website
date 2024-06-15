e78f6a0172fe7df0bacba65614aba4b817f2fb5a
vishnu prasanth <kvishnuprasanth2@gmail.com>
2024-06-15 20:14:36 +0530

    apis

--- backend/routes/autherization.js
+++ backend/routes/autherization.js
@@ -9,11 +9,11 @@ router.post("/register", async (req,res)=>{
         const {firstName, lastName, email,contactNo, password} = req.body;
         const hashpassword = bcrypt.hashSync(password)
         const user = new User({firstName, lastName, email,contactNo, password: hashpassword})
-        await user.save().then((res)=>{
+        await user.save();
             
             res.status(200).json({zooma: user})
             console.log('ofiysoifd')
-        })
+        
     } catch (error) {
         res.status(400).json({message: "user already exists"})
     }
