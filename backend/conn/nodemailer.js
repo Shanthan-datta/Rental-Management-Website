c6a26b14cd9888c509af30be22ff5f3156a55df8
vishnu prasanth <kvishnuprasanth2@gmail.com>
2024-07-07 10:11:53 +0530

    ok

--- backend/conn/nodemailer.js
+++ backend/conn/nodemailer.js
@@ -1,1 +1,15 @@
-const transport 
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.AUTH_MAILER_EMAIL,
+        pass: process.env.AUTH_MAILER_PASS
+    }
+
+  });
+
+  module.exports = {
+    transporter: transporter
+}
